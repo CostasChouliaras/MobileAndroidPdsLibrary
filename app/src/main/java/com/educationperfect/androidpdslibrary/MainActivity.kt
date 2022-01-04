4697927258d3a546af52054346add927e9c9339c
Costas Chouliaras <costas.chouliaras@educationperfect.com>
2022-01-04 12:43:48 +0200

    Refactor

--- app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
+++ app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.educationperfect.androidpdslibrary.ui.theme.AndroidPdsLibraryTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                    ACustomComposable()
                 }
             }
         }
