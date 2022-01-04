61d754241b461e87afd0b351c4fab289ddc2a12f
Costas Chouliaras <costas.chouliaras@educationperfect.com>
2022-01-04 11:51:50 +0200

    Initial commit

---
+++ pds-library/src/main/java/com/educationperfect/pds_library/components/ACustomComposable.kt
@@ -0,0 +1,33 @@
+package com.educationperfect.pds_library.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ACustomComposable()
+{
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text =  "This is a text",
+            modifier = Modifier.fillMaxSize(),
+            color = Color.Cyan
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text =  "Click me",
+                modifier = Modifier.fillMaxSize(),
+                color = Color.Cyan
+            )
+        }
+    }
+}
