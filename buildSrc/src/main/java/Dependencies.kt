7ed82d2ac87eb603ecc8bfe42a8d09268766c61c
Costas Chouliaras <costas.chouliaras@educationperfect.com>
2022-01-06 15:36:53 +0200

    Test

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -12,7 +12,7 @@ object Libs {
     const val junit = "junit:junit:4.13"
 
     object Kotlin {
-        const val version = "1.5.31"
+        const val version = "1.5.30"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
