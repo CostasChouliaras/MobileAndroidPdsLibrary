3aa399df5ee4b91f858094d9419be0fffadb5b58
Costas Chouliaras <costas.chouliaras@educationperfect.com>
2022-01-07 14:12:29 +0200

    Fixed remote

--- app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
+++ app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    EpActivityButton(text = "smth", onCLick = { /*TODO*/ }, modifier =  Modifier.fillMaxSize())
+                    EpActivityButton(text = "Click!", onCLick = { /*TODO*/ }, modifier =  Modifier.fillMaxSize())
                 }
             }
         }
