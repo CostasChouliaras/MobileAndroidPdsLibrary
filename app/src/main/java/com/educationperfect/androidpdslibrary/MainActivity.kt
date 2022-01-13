5b593797e19c6225234a65d563c68d56a1a8eef7
Costas Chouliaras <costas.chouliaras@educationperfect.com>
2022-01-13 12:40:08 +0200

    Excluded unnecessary dependencies from app module. Imported only necessary dependencies to library module

--- app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
+++ app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
@@ -3,15 +3,9 @@ package com.educationperfect.androidpdslibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.educationperfect.androidpdslibrary.ui.theme.AndroidPdsLibraryTheme
-import com.educationperfect.pds_library.components.EpActivityButton
+
 
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +14,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidPdsLibraryTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
 
-                    EpActivityButton(text = "Click!", onCLick = { /*TODO*/ }, modifier =  Modifier.fillMaxSize())
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidPdsLibraryTheme {
-        Greeting("Android")
     }
 }
