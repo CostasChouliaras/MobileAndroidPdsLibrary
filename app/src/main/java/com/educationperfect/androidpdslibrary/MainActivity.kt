fef618e3c12bd9bf1b55b9d61ad10e9305b7403c
Costas Chouliaras <costas.chouliaras@educationperfect.com>
2022-02-22 11:40:15 +0200

    Fixed Avatar.

--- app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
+++ app/src/main/java/com/educationperfect/androidpdslibrary/MainActivity.kt
@@ -5,17 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
-import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowForwardIos
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
 import com.educationperfect.androidpdslibrary.ui.theme.AndroidPdsLibraryTheme
-import com.educationperfect.pds_library.components.EdsTag
-import com.educationperfect.pds_library.components.EpTabRow
-import com.educationperfect.pds_library.components.OutlinedTextFieldWithFixedLabel
-import com.educationperfect.pds_library.components.PlainTextFieldWithFixedLabel
+import com.educationperfect.pds_library.components.Avatar
 import com.educationperfect.pds_library.ui.Neutral300
 
 
@@ -31,9 +26,8 @@ class MainActivity : ComponentActivity() {
                     tint = Neutral300,
                     modifier = Modifier.width(16.dp)
                 )
-                EpTabRow(items = listOf("Ste", "tst", "sts") , tabIndex = 0 , onClick =  {} )
+                Avatar(initials = "KP", isOnline = true, height = 100.dp, width = 100.dp)
             }
-
         }
     }
 }
